0642db64fdd0d52c06c348c839ca81a4bdaf62d3
Benjamin Soung <soungbenjamin@gmail.com>
2017-09-25 18:02:09 -0400

    knight searcher

--- chess.js
+++ chess.js
@@ -1,5 +1,7 @@
+const Queue = require('./queue');
+
 class Move {
-	constructor(x, y, depth, children=[], parent) {
+	constructor(x, y, depth, children = [], parent) {
 		this.x = x;
 		this.y = y;
 		this.depth = depth;
@@ -12,16 +14,21 @@ class Move {
 // 2nd is maxDepth of tree
 class MoveTree {
 	constructor(coordinates, maxDepth) {
-		this.maxDepth = maxDepth
+		this.maxDepth = maxDepth;
 		this.root = new Move(coordinates[0], coordinates[1], 0, [], null);
-//		for (let i = 0; i < maxDepth; i++) {
-		this.nodeCount = 1
+		//		for (let i = 0; i < maxDepth; i++) {
+		this.nodeCount = 1;
 		this._buildTree();
-//		}
+		//		}
 	}
 
 	inspect() {
-		console.log("This tree has ", this.nodeCount, " nodes and a maximum depth of ", this.maxDepth)
+		console.log(
+			'This tree has ',
+			this.nodeCount,
+			' nodes and a maximum depth of ',
+			this.maxDepth
+		);
 	}
 
 	_buildTreeRecursive(move = null) {
@@ -31,12 +38,12 @@ class MoveTree {
 	}
 
 	_buildTree(move = this.root) {
-		if (move.depth === this.maxDepth) return 
+		if (move.depth === this.maxDepth) return;
 		let movesArr = this._createMoves(move);
 		movesArr.forEach((m, i) => {
-			this.nodeCount++
+			this.nodeCount++;
 			move.children[i] = m;
-			this._buildTree(m)
+			this._buildTree(m);
 		});
 	}
 
@@ -65,9 +72,52 @@ class MoveTree {
 			moveSix,
 			moveSeven,
 			moveEight
-		].filter(m => m.x >= 0 && m.y >= 0 && m.x<=7 && m.y<=7);
+		].filter(m => m.x >= 0 && m.y >= 0 && m.x <= 7 && m.y <= 7);
 	}
 }
-let moveTree = new MoveTree([6, 0], 2)
-moveTree.inspect()
+
+class KnightSearcher {
+	constructor(tree) {
+		this.tree = tree;
+	}
+
+	bfsFor(coordinates) {
+		let queue = new Queue();
+		let nodesCoordinates = [];
+		let n = this.tree.root;
+
+		function traverseUp(node) {
+			if (node.parent === null) {
+				nodesCoordinates.push([node.x, node.y]);
+				console.log(
+					`${nodesCoordinates.length - 1} Moves: ${nodesCoordinates.reverse()}`
+				);
+				return;
+			}
+
+			nodesCoordinates.push([node.x, node.y]);
+			traverseUp(node.parent);
+		}
+
+		queue.enqueue(n);
+
+		while (queue.length > 0) {
+			n = queue.dequeue();
+
+			if (n.x === coordinates[0] && n.y === coordinates[1]) {
+				traverseUp(n);
+			}
+		}
+	}
+}
+
+let moveTree = new MoveTree([6, 0], 2);
+moveTree.inspect();
 // console.log(moveTree.root.children[0])
+//
+// This tree has  22  nodes and a maximum depth of  2
+//
+//
+//
+//
+//
